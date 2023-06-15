6c02c234f7b1c672f7d9025820fff1f1b03e4275
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-06-15 14:14:28 +0300

    phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance) {
+      if (distance <= 199) {
+        return "This one is on me!";
+      } else if (distance <= 2001) {
+        return "I will gladly take your thirty bucks.";
+      } else if (distance <= 2501) {
+        return "No can do.";
+      } else {
+        return "Whoa, that's quite a journey!";
+      }
+    }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function ternaryCheckCity(destination) {
+      return destination === "NYC" ? "Ok, sounds good." : "No go.";
+    }
+    
+    
+function switchOnCharmFromTip(maxAmount) {
+      switch (maxAmount) {
+        case "generous":
+          return "Thank you so much.";
+        case "not as generous":
+          return "Thank you.";
+        default:
+        return "Bye.";
+        
+      }
+    }
